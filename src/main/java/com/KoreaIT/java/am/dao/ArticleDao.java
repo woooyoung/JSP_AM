fca47915db69410e11707a63879ad375b596afe8
woooyoung <axdsw121@gmail.com>
2022-09-22 17:34:23 +0900

    articleDao 도입(List)

---
+++ src/main/java/com/KoreaIT/java/am/dao/ArticleDao.java
@@ -0,0 +1,38 @@
+package com.KoreaIT.java.am.dao;
+
+import java.sql.Connection;
+import java.util.List;
+import java.util.Map;
+
+import com.KoreaIT.java.am.util.DBUtil;
+import com.KoreaIT.java.am.util.SecSql;
+
+public class ArticleDao {
+	private Connection conn;
+
+	public ArticleDao(Connection conn) {
+		this.conn = conn;
+	}
+
+	public int getTotalCount() {
+		SecSql sql = SecSql.from("SELECT COUNT(*) AS cnt");
+		sql.append("FROM article");
+
+		return DBUtil.selectRowIntValue(conn, sql);
+	}
+
+	public List<Map<String, Object>> getArticleRows(int itemsInAPage, int limitFrom) {
+
+		SecSql sql = SecSql.from("SELECT A.*, M.name AS writer");
+		sql.append("FROM article AS A");
+		sql.append("INNER JOIN `member` AS M");
+		sql.append("ON A.memberId = M.id");
+		sql.append("ORDER BY A.id DESC");
+		sql.append("LIMIT ?, ?", limitFrom, itemsInAPage);
+
+		List<Map<String, Object>> articleRows = DBUtil.selectRows(conn, sql);
+
+		return articleRows;
+	}
+
+}
--- src/main/java/com/KoreaIT/java/am/service/ArticleService.java
+++ src/main/java/com/KoreaIT/java/am/service/ArticleService.java
@@ -4,14 +4,15 @@ import java.sql.Connection;
 import java.util.List;
 import java.util.Map;
 
+import com.KoreaIT.java.am.dao.ArticleDao;
 import com.KoreaIT.java.am.util.DBUtil;
 import com.KoreaIT.java.am.util.SecSql;
 
 public class ArticleService {
-	private Connection conn;
+	private ArticleDao articleDao;
 
 	public ArticleService(Connection conn) {
-		this.conn = conn;
+		articleDao = new ArticleDao(conn);
 	}
 
 	public int getitemsInAPage() {
@@ -22,10 +23,8 @@ public class ArticleService {
 
 		int itemsInAPage = getitemsInAPage();
 
-		SecSql sql = SecSql.from("SELECT COUNT(*) AS cnt");
-		sql.append("FROM article");
+		int totalCount = articleDao.getTotalCount();
 
-		int totalCount = DBUtil.selectRowIntValue(conn, sql);
 		int totalPage = (int) Math.ceil((double) totalCount / itemsInAPage);
 
 		return totalPage;
@@ -38,14 +37,7 @@ public class ArticleService {
 
 		int limitFrom = (page - 1) * itemsInAPage;
 
-		SecSql sql = SecSql.from("SELECT A.*, M.name AS writer");
-		sql.append("FROM article AS A");
-		sql.append("INNER JOIN `member` AS M");
-		sql.append("ON A.memberId = M.id");
-		sql.append("ORDER BY A.id DESC");
-		sql.append("LIMIT ?, ?", limitFrom, itemsInAPage);
-
-		List<Map<String, Object>> articleRows = DBUtil.selectRows(conn, sql);
+		List<Map<String, Object>> articleRows = articleDao.getArticleRows(itemsInAPage, limitFrom);
 
 		return articleRows;
 	}
